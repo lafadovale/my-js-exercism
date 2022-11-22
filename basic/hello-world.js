8a70c91d48d6b87850a02a88a55a098bb6c86ba4
Lafaiete do Vale <lafa.vale@gmail.com>
2022-11-22 00:13:33 -0300

    Mother of all functions

---
+++ basic/hello-world.js
@@ -0,0 +1,9 @@
+// The classical introductory exercise. Just say "Hello, World!".
+// "Hello, World!" is the traditional first program for beginning programming in a new language or environment.
+// The objective is simple:
+
+// Write a function that returns the string "Hello, World!".
+
+export function hello() {
+  return 'Hello, World!';
+}
